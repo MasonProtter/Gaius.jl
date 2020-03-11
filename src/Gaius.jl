90c829221f1c074b41b603848d10bb12b034510f
MasonProtter <mason.protter@icloud.com>
2020-03-11 13:08:01 -0600

    early setup

--- src/Gaius.jl
+++ src/Gaius.jl
@@ -14,6 +14,9 @@ const MatTypesC{T <: Eltypes} = Union{Matrix{T}, SubArray{T, 2, <: Array}} # C f
 const MatTypesR{T <: Eltypes} = Union{LinearAlgebra.Adjoint{T,<:MatTypesC{T}}, LinearAlgebra.Transpose{T,<:MatTypesC{T}}} # R for Row Major
 const MatTypes{T <: Eltypes} = Union{MatTypesC{T}, MatTypesR{T}}
 
+const   VecTypes{T <: Eltypes} = Union{Vector{T}, SubArray{T, 1, <:Array}}
+const CoVecTypes{T <: Eltypes} = Union{LinearAlgebra.Adjoint{T,<:VecTypes{T}}, LinearAlgebra.Transpose{T, <:VecTypes{T}}}
+
 # Note this does not support changing the number of threads at runtime
 macro _spawn(ex)
     if Threads.nthreads() > 1
--- src/matmul.jl
+++ src/matmul.jl
@@ -13,11 +13,19 @@ function blocked_mul(A::MatTypes, B::MatTypes)
     C
 end
 
+function blocked_mul(A::StructArray{Complex{T}, 2}, B::StructArray{Complex{T}, 2}) where {T <: Eltypes}
+    C = StructArray{Complex{T}}((Matrix{T}(undef, size(A, 1), size(B,2)),
+                                 Matrix{T}(undef, size(A, 1), size(B,2))))
+    blocked_mul!(C, A, B)
+    C
+end
+
 choose_block_size(C::AbstractMatrix, ::Nothing) = size(C, 1) >= (3DEFAULT_BLOCK_SIZE) >>> 1 ? DEFAULT_BLOCK_SIZE : 32
 choose_block_size(::Any, block_size::Integer) = block_size
 
+
 function blocked_mul!(C::MatTypes{T}, A::MatTypes{T}, B::MatTypes{T};
-              block_size = nothing, sizecheck=true) where {T <: Eltypes}
+                      block_size = nothing, sizecheck=true) where {T <: Eltypes}
     sizecheck && check_compatible_sizes(C, A, B)
 
     _block_size = choose_block_size(C, block_size)
@@ -26,15 +34,8 @@ function blocked_mul!(C::MatTypes{T}, A::MatTypes{T}, B::MatTypes{T};
     C
 end
 
-function blocked_mul(A::StructArray{Complex{T}, 2}, B::StructArray{Complex{T}, 2}) where {T <: Eltypes}
-    C = StructArray{Complex{T}}((Matrix{T}(undef, size(A, 1), size(B,2)),
-                                 Matrix{T}(undef, size(A, 1), size(B,2))))
-    blocked_mul!(C, A, B)
-    C
-end
-
 function blocked_mul!(C::StructArray{Complex{T}, 2}, A::StructArray{Complex{T}, 2}, B::StructArray{Complex{T}, 2};
-              block_size = DEFAULT_BLOCK_SIZE, sizecheck=true) where {T <: Eltypes}
+                      block_size = DEFAULT_BLOCK_SIZE, sizecheck=true) where {T <: Eltypes}
     sizecheck && check_compatible_sizes(C, A, B)
     
     _block_size = choose_block_size(C, block_size)
@@ -43,9 +44,6 @@ function blocked_mul!(C::StructArray{Complex{T}, 2}, A::StructArray{Complex{T}, 
         Cre, Cim = PtrMatrix(C.re), PtrMatrix(C.im)
         Are, Aim = PtrMatrix(A.re), PtrMatrix(A.im)
         Bre, Bim = PtrMatrix(B.re), PtrMatrix(B.im)
-        # Cre, Cim = C.re, C.im
-        # Are, Aim = A.re, A.im
-        # Bre, Bim = B.re, B.im
         _mul!(    Cre, Are, Bre, _block_size)          # C.re = A.re * B.re
         _mul_add!(Cre, Aim, Bim, _block_size, Val(-1)) # C.re = C.re - A.im * B.im
         _mul!(    Cim, Are, Bim, _block_size)          # C.im = A.re * B.im
@@ -87,3 +85,4 @@ function _mul_add!(C, A, B, sz, ::Val{factor} = Val(1)) where {factor}
         add_gemm_kernel!(C, A, B, Val(factor))
     end
 end
+
