ce3972b36547e2d7344a69e80a106e6ab85a67f2
Dilum Aluthge <dilum@aluthge.com>
2020-12-20 19:22:17 -0500

    Put all testsets in a single top-level testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,8 @@ using Test
 
 versioninfo(; verbose = true)
 
+@testset "Gaius.jl" begin
+
 @testset "Float64 Matrix-Vector       " begin
     for n ∈ [10, 100, 500, 10000]
         for m ∈ [10, 100, 10000]
@@ -227,3 +229,5 @@ end
         end
     end
 end
+
+end # end @testset "Gaius.jl"
