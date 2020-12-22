cd862226f1b202bd1a4a9a0a2c0ac59a2b7c8b8c
Dilum Aluthge <dilum@aluthge.com>
2020-12-21 19:16:42 -0500

    Print some useful information during the test suite (#31)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,17 @@
-using Test, LinearAlgebra, Random
 using Gaius
-using StructArrays
+using LinearAlgebra
+using Random
+using Test
 
-@testset "Float64 Matrix-Vector       " begin
+import BenchmarkTools
+import InteractiveUtils
+import StructArrays
+
+InteractiveUtils.versioninfo()
+
+@info("Running tests with $(Threads.nthreads()) threads")
+
+@time @testset "Float64 Matrix-Vector" begin
     for n ∈ [10, 100, 500, 10000]
         for m ∈ [10, 100, 10000]
             u = zeros(n)
@@ -14,19 +23,19 @@ using StructArrays
     end
 end
 
-@testset "ComplexF64  Matrix-Vector   " begin
+@time @testset "ComplexF64  Matrix-Vector" begin
     for n ∈ [10, 100, 500, 2000]
         for m ∈ [10, 100, 2000]
-            u = zeros(ComplexF64, n)   |> StructArray
-            A = rand(ComplexF64, n, m) |> StructArray
-            v = rand(ComplexF64, m)    |> StructArray
+            u = zeros(ComplexF64, n)   |> StructArrays.StructArray
+            A = rand(ComplexF64, n, m) |> StructArrays.StructArray
+            v = rand(ComplexF64, m)    |> StructArrays.StructArray
             @test blocked_mul!(u, A, v) ≈ collect(A) * collect(v)
             @test u ≈ blocked_mul(A, v)
         end
     end
 end
 
-@testset "Float32 Matrix-Vector       " begin
+@time @testset "Float32 Matrix-Vector" begin
     for n ∈ [10, 100, 500, 2000]
         for m ∈ [10, 100, 2000]
             u = zeros(Float32, n)
@@ -38,7 +47,7 @@ end
     end
 end
 
-@testset "Int64 Matrix-Vector         " begin
+@time @testset "Int64 Matrix-Vector" begin
     for n ∈ [10, 100, 500, 2000]
         for m ∈ [10, 100, 2000]
             u = zeros(Int, n)
@@ -50,12 +59,12 @@ end
     end
 end
 
-@testset "ComplexInt32 Matrix-Vector  " begin
+@time @testset "ComplexInt32 Matrix-Vector" begin
     for n ∈ [10, 100, 500, 2000]
         for m ∈ [10, 100, 2000]
-            u = zeros(Complex{Int32}, n) |> StructArray       
-            A = StructArray{Complex{Int32}}((rand(Int32.(-10:10), n, m), rand(Int32.(-10:10), n, m)))
-            v = StructArray{Complex{Int32}}((rand(Int32.(-10:10),    m), rand(Int32.(-10:10),    m)))   
+            u = zeros(Complex{Int32}, n) |> StructArrays.StructArray
+            A = StructArrays.StructArray{Complex{Int32}}((rand(Int32.(-10:10), n, m), rand(Int32.(-10:10), n, m)))
+            v = StructArrays.StructArray{Complex{Int32}}((rand(Int32.(-10:10),    m), rand(Int32.(-10:10),    m)))
             @test blocked_mul!(u, A, v) ≈ collect(A) * collect(v)
             @test u ≈ blocked_mul(A, v)
         end
@@ -63,7 +72,7 @@ end
 end
 
 
-@testset "Float64 CoVector-Matrix     " begin
+@time @testset "Float64 CoVector-Matrix" begin
     for n ∈ [10, 100, 500, 2000]
         for m ∈ [10, 100, 2000]
             u = zeros(m)
@@ -78,12 +87,12 @@ end
     end
 end
 
-@testset "ComplexF32 CoVector-Matrix  " begin
+@time @testset "ComplexF32 CoVector-Matrix" begin
     for n ∈ [10, 100, 500, 2000]
         for m ∈ [10, 100, 2000]
-            u = zeros(Complex{Float32}, m)   |> StructArray
-            A = rand(Complex{Float32}, n, m) |> StructArray
-            v = rand(Complex{Float32}, n)    |> StructArray
+            u = zeros(Complex{Float32}, m)   |> StructArrays.StructArray
+            A = rand(Complex{Float32}, n, m) |> StructArrays.StructArray
+            v = rand(Complex{Float32}, n)    |> StructArrays.StructArray
             @test blocked_mul!(u', v', A) ≈ v' * A
             @test u' ≈ blocked_mul(v', A)
 
@@ -96,15 +105,15 @@ end
 
 
 
-@testset "ComplexFloat64 Matrix-Matrix" begin
+@time @testset "ComplexFloat64 Matrix-Matrix" begin
     for sz ∈ [10, 50, 100, 200, 400, 1000]
         n, k, m = (sz .+ rand(-5:5, 3))
         @testset "($n × $k) × ($k × $m)" begin
-            C1 = StructArray{ComplexF64}((zeros(n, m), zeros(n, m))) 
-            C2 = StructArray{ComplexF64}((zeros(n, m), zeros(n, m))) 
-            A  = StructArray{ComplexF64}((randn(n, k), randn(n, k))) 
-            B  = StructArray{ComplexF64}((randn(k, m), randn(k, m)))
-           
+            C1 = StructArrays.StructArray{ComplexF64}((zeros(n, m), zeros(n, m)))
+            C2 = StructArrays.StructArray{ComplexF64}((zeros(n, m), zeros(n, m)))
+            A  = StructArrays.StructArray{ComplexF64}((randn(n, k), randn(n, k)))
+            B  = StructArrays.StructArray{ComplexF64}((randn(k, m), randn(k, m)))
+
             @test blocked_mul!(C1, A, B) ≈ mul!(C2, A, B)
             @test blocked_mul(A, B) ≈ C1
         end
@@ -112,18 +121,18 @@ end
     for sz ∈ [10, 50, 100, 200, 400, 1000]
         n, k, m = shuffle([sz + rand(-5:5), sz + rand(-5:5), 10])
         @testset "($n × $k) × ($k × $m)" begin
-            C1 = StructArray{ComplexF64}((zeros(n, m), zeros(n, m))) 
-            C2 = StructArray{ComplexF64}((zeros(n, m), zeros(n, m))) 
-            A  = StructArray{ComplexF64}((randn(n, k), randn(n, k))) 
-            B  = StructArray{ComplexF64}((randn(k, m), randn(k, m)))
-           
+            C1 = StructArrays.StructArray{ComplexF64}((zeros(n, m), zeros(n, m)))
+            C2 = StructArrays.StructArray{ComplexF64}((zeros(n, m), zeros(n, m)))
+            A  = StructArrays.StructArray{ComplexF64}((randn(n, k), randn(n, k)))
+            B  = StructArrays.StructArray{ComplexF64}((randn(k, m), randn(k, m)))
+
             @test blocked_mul!(C1, A, B) ≈ mul!(C2, A, B)
             @test blocked_mul(A, B) ≈ C1
         end
     end
 end
 
-@testset "Float64 Matrix-Matrix       " begin
+@time @testset "Float64 Matrix-Matrix" begin
     for sz ∈ [10, 50, 100, 200, 400, 1000]
         n, k, m = (sz .+ rand(-5:5, 3))
         @testset "($n × $k) × ($k × $m)" begin
@@ -133,10 +142,10 @@ end
             B  = randn(k, m)
             At = copy(A')
             Bt = copy(B')
-            
+
             @test blocked_mul!(C1, A, B) ≈ mul!(C2, A, B)
             @test blocked_mul(A, B) ≈ C1
-            
+
             fill!(C1, NaN); @test blocked_mul!(C1, At', B)   ≈ C2
             fill!(C1, NaN); @test blocked_mul!(C1, A,   Bt') ≈ C2
             fill!(C1, NaN); @test blocked_mul!(C1, At', Bt') ≈ C2
@@ -151,7 +160,7 @@ end
             B  = randn(k, m)
             At = copy(A')
             Bt = copy(B')
-           
+
             @test blocked_mul!(C1, A, B) ≈ mul!(C2, A, B)
             @test blocked_mul(A, B) ≈ C1
             fill!(C1, NaN); @test blocked_mul!(C1, At', B)   ≈ C2
@@ -161,7 +170,7 @@ end
     end
 end
 
-@testset "Float32 Matrix-Matrix       " begin
+@time @testset "Float32 Matrix-Matrix" begin
     for sz ∈ [10, 50, 100, 200, 400, 1000]
         n, k, m = (sz .+ rand(-5:5, 3))
         @testset "($n × $k) × ($k × $m)" begin
@@ -171,7 +180,7 @@ end
             B  = randn(Float32, k, m)
             At = copy(A')
             Bt = copy(B')
-           
+
             @test blocked_mul!(C1, A, B) ≈ mul!(C2, A, B)
             @test blocked_mul(A, B) ≈ C1
             fill!(C1, NaN32); @test blocked_mul!(C1, At', B)   ≈ C2
@@ -182,7 +191,7 @@ end
 end
 
 
-@testset "Int64 Matrix-Matrix         " begin
+@time @testset "Int64 Matrix-Matrix" begin
     for sz ∈ [10, 50, 100, 200, 400, 1000]
         n, k, m = (sz .+ rand(-5:5, 3))
         @testset "($n × $k) × ($k × $m)" begin
@@ -192,7 +201,7 @@ end
             B  = rand(-100:100, k, m)
             At = copy(A')
             Bt = copy(B')
-           
+
             @test blocked_mul!(C1, A, B) ≈ mul!(C2, A, B)
             @test blocked_mul(A, B) ≈ C1
             fill!(C1, typemax(Int64)); @test blocked_mul!(C1, At', B)   ≈ C2
@@ -203,7 +212,7 @@ end
 end
 
 
-@testset "Int32 Matrix-Matrix         " begin
+@time @testset "Int32 Matrix-Matrix" begin
     for sz ∈ [10, 50, 100, 200, 400, 1000]
         n, k, m = (sz .+ rand(-5:5, 3))
         @testset "($n × $k) × ($k × $m)" begin
